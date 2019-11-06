ac65e619415b7e3ae25cfedc58ef69d1a95a83cd
Lucas <lucasdammel@gmail.com>
2019-11-06 09:53:07 +0100

    added VFX, Post-Proccessing & models

---
+++ Assets/Scripts/Library/Combat/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Library.Combat
+{
+    public class LookAt : MonoBehaviour
+    {
+        [SerializeField] private GameObject target;
+
+        private void Start()
+        {
+            target = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            transform.LookAt(target.transform);
+        }
+    }
+}
