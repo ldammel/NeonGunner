851555000cb0f2e3b5ea8171b3a6486ba7faa2bc
Lucas <lucasdammel@gmail.com>
2020-01-14 14:34:05 +0100

    started Checkpoint

---
+++ Assets/Scripts/Library/Events/Checkpoint/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BansheeGz.BGSpline.Components;
+using TMPro;
+using UnityEngine;
+
+namespace Library.Events.Checkpoint
+{
+    public class Checkpoint : MonoBehaviour
+    {
+
+        [SerializeField] private BGCcTrs path;
+        [SerializeField] private float timeToStop;
+
+        private bool _activated;
+        private float _curSpeed;
+
+        private void Start()
+        {
+            _curSpeed = path.Speed;
+        }
+
+        private void Update()
+        {
+            if (!_activated) return;
+            path.Speed = Mathf.Lerp(_curSpeed, 0, timeToStop);
+
+        }
+    }
+}
