02664b9180f44a9d7db5effcc474e1c8d820d1fc
Lucas <lucasdammel@gmail.com>
2020-02-13 18:30:41 +0100

    official

---
+++ Assets/Scripts/SoundSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSettings : MonoBehaviour
+{
+    public Slider music;
+    public Slider sfx;
+    public AudioMixer mixer;
+
+    private void Update()
+    {
+        mixer.SetFloat("Music", music.value);
+        mixer.SetFloat("SFX", sfx.value);
+    }
+}
